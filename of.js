879da56937bbd28fb0fc41dd2aa7fb9e03ae81ef
Flyinvap <flyinvap@kabano.net>
2014-01-08 22:32:55 +0100

    Corrections d'erreur jslint

--- of.js
+++ of.js
@@ -20,39 +20,40 @@ Lancement : node of.js
 
 */
 
+
+
 var fs = require('fs');
 var http = require('http');
 var request = require('request');
 var rss = require('node-rss');
-require("/usr/local/lib/node_modules/node-codein");
+//require("/usr/local/lib/node_modules/node-codein");
 
-var settings = JSON.parse(fs.readFileSync('config.json', encoding="ascii"));
-httpPort = settings.httpPort || 8001;
-httpHost = settings.httpHost || "127.0.0.1";
+var settings = JSON.parse(fs.readFileSync('config.json', encoding = "ascii"));
+var httpPort = settings.httpPort || 8001;
+var httpHost = settings.httpHost || "127.0.0.1";
 var cacheDuration = settings.cacheDuration || 120;
 
 var httpServer = http.createServer(onRequest);
 httpServer.listen(httpPort, httpHost);
-console.log("Server running at http://"+ httpHost + ":" + httpPort);
+console.log("Server running at http://" + httpHost + ":" + httpPort);
 
 function onRequest(request, response) {
 
 	fs.stat(settings.feedsDir + request.url, function (err, stats) {
 		if (err) {
-			if (settings.debug) console.log('create feed');
-			createFeed( settings, request, response, function() {sendFeed(settings, request, response);} );
+			if (settings.debug) { console.log('create feed'); }
+			createFeed(settings, request, response, function () {sendFeed(settings, request, response); });
 		} else {
-			var now = Date.now();
-			var fileDuration = (now - stats.ctime) / 1000 / 60;
+			var fileDuration = (Date.now() - stats.ctime) / 1000 / 60;
 
-			if (settings.debug) console.log("file created " + fileDuration + " min ago");
+			if (settings.debug) { console.log("file created " + fileDuration + " min ago"); }
 
-			if ( fileDuration > settings.cacheDuration ) {
-				createFeed(settings, request, response, function() {sendFeed(settings, request, response);});
+			if (fileDuration > settings.cacheDuration) {
+				createFeed(settings, request, response, function () {sendFeed(settings, request, response); });
 			} else {
 				sendFeed(settings, request, response);
 			}
-		} 
+		}
 	});
 }
 
@@ -63,76 +64,79 @@ function onRequest(request, response) {
 */
 
 function createFeed(settings, httpRequest, httpResponse, callback) {
-	var city = httpRequest.url;
-	city = city.replace(/\//gi, "");
-	var httpHost =  httpRequest.headers['x-forwarded-host'] || httpRequest.headers.host;
-	var siteUrl = settings.urlBase + settings.urlPathBase + city;
-	var feedLink = 'http://' + httpHost + httpRequest.url;
-	
-	var feed = newFeedHeader(settings, city, siteUrl, feedLink);
+    var city, httpHost, siteUrl, feedLink, feed;
+    
+	city = httpRequest.url.replace(/\//gi, "");
+	httpHost =  httpRequest.headers['x-forwarded-host'] || httpRequest.headers.host;
+	siteUrl = settings.urlBase + settings.urlPathBase + city;
+	feedLink = 'http://' + httpHost + httpRequest.url;
+	feed = newFeedHeader(settings, city, siteUrl, feedLink);
 
 	console.log('GET: ' + siteUrl);
 
 	// request page, parse and add new feed items
 	request(siteUrl, function (error, response, body) {
 		extractArticles(error, response, body, settings, httpRequest,  feed, callback);
-	})
+	});
 }
 
-function newFeedHeader (settings, city, url, link) {
+function newFeedHeader(settings, city, url, link) {
 	// Define new feed options
-	var feed = rss.createNewFeed(
-								'Ouest France ' + city, url,
-								'',
-								'Flyinva <flyinva@kabano.net>',
-								link, 
-								{language : settings.feedLang }
-								);
+    var feed = rss.createNewFeed(
+        'Ouest France ' + city,
+        url,
+		'',
+		'Flyinva <flyinva@kabano.net>',
+        link,
+        {language : settings.feedLang }
+    );
 
-	return(feed);
+	return (feed);
 
 }
 
-function extractArticles (error, response, body, settings, request, feed, callback) {
-	if (!error && response.statusCode == 200) {
+function extractArticles(error, response, body, settings, request, feed, callback) {
+	if (!error && response.statusCode === 200) {
 		var cheerio = require('cheerio'), $ = cheerio.load(body);
 		
-		$('article').each( function(i, elem) { 
-			htmlToFeed(i, elem, feed, this)
+		$('article').each(function (i, elem) {
+			htmlToFeed(i, elem, feed, this);
 		});
 
-		if (settings.debug) console.log('write: ' + settings.feedsDir + request.url);
+		if (settings.debug) { console.log('write: ' + settings.feedsDir + request.url); }
 		
-		var xmlFeed = rss.getFeedXML(feed);
-		writeFeedToFile(settings.feedsDir, request.url, xmlFeed, callback);
+		//var xmlFeed = rss.getFeedXML(feed);
+		writeFeedToFile(settings.feedsDir, request.url, rss.getFeedXML(feed), callback);
 
 	}
 }
 
-function htmlToFeed(i, elem, feed, article){
+function htmlToFeed(i, elem, feed, article) {
 	//console.log(that);
-	var description = article.find('p').text();
+    var description, title, url, time;
+    
+	description = article.find('p').text();
 	description = description.replace(/[\r\n]/gm, "");
 	description = description.replace(/\s{2,}/g, "");
 	
-	var title = article.find('h2').text();
+	title = article.find('h2').text();
 	title = title.replace(/[\r\n]\s{2,}/gm, "");
 	title = title.replace(/\s{2,}/g, "");
 	
-	var url = article.find('a').attr('href');
-	var time = article.find('time').attr('datetime');
+	url = article.find('a').attr('href');
+	time = article.find('time').attr('datetime');
 	
 	feed.addNewItem(title, settings.urlBase + url, time, description, {});
 }
 
-function writeFeedToFile (dir, url, feed, callback) {
-	fs.writeFile(
-			dir + url, 
-			feed, 
-			function (err) {
-				if (err) throw err;
-				callback();
-			}
+function writeFeedToFile(dir, url, feed, callback) {
+    fs.writeFile(
+        dir + url,
+        feed,
+        function (err) {
+            if (err) { throw err; }
+            callback();
+		}
 	);
 }
 
@@ -140,6 +144,6 @@ function sendFeed(settings, httpRequest, httpResponse) {
 	feed = fs.readFileSync(settings.feedsDir + httpRequest.url, 'utf8');
 	httpResponse.writeHead(200, {'Content-Type': 'application/x-rss+xml'});
 	httpResponse.end(feed);
-	if (settings.debug) console.log('feed sent from file');
+	if (settings.debug) { console.log('feed sent from file'); }
 }
 
